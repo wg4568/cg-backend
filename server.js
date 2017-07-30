873a9a7c9bc51b9de098cf917057094ee3f8cb1c
William Gardner <wg4568@gmail.com>
2017-07-29 17:36:07 -0700

    changed port

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+	res.send('hello world test 2');
+})
+
+app.listen(8080, function () {
+	console.log('Example app listening on port 3000!')
+})
