29fed19c0da1937ae06aa75822a30f1731f9601e
William Gardner <wg4568@gmail.com>
2017-07-29 17:40:56 -0700

    fixed formatting

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
 	res.send('hello world test 2');
-})
+});
 
 app.listen(8080, function () {
-	console.log('Example app listening on port 3000!')
-})
+	console.log('Example app listening on port 3000!');
+});
